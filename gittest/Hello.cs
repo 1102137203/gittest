d7a40423ae7eb730f0378577cda55bdb923b5390
1102137203 <1102137203@gm.kuas.edu.tw>
2016-03-03 15:05:22 +0800

    add hello.cs

---
+++ gittest/Hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gittest
+{
+    class Hello
+    {
+        public void main() {
+            Console.WriteLine("123");
+            Console.ReadKey();
+        }
+    }
+}
